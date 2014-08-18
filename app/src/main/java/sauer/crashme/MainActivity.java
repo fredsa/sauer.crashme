bbdadb2eb8828ad7a164ec28cb5a843043ae6884
Fred Sauer <fredsa@google.com>
2014-08-18 14:06:37 -0700

    add NPE crash button

--- app/src/main/java/sauer/crashme/MainActivity.java
+++ app/src/main/java/sauer/crashme/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +14,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button npeButton = (Button) findViewById(R.id.button_npe);
+        npeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Object foo = null;
+                foo.hashCode();
+            }
+        });
     }
 
 
